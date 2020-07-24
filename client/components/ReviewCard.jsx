5ceeedcccf36a12e93f702a7f1778ccedeb8aba8
haejinjo <h3jnjo@gmail.com>
2020-07-23 19:18:33 -0700

    Display resident name instead of Resident Number X

--- client/components/ReviewCard.jsx
+++ client/components/ReviewCard.jsx
@@ -4,8 +4,28 @@ class ReviewCard extends Component {
   constructor(props) {
     super(props)
 
+    this.state = {
+      residents: null,
+    }
+
     this.onDelete = this.onDelete.bind(this);
     this.onEdit = this.onEdit.bind(this);
+  }
+
+  componentDidMount() {
+
+    // Get all residents as {username: x, _id: y} objects to display in dropdown
+    fetch('/api/residents')
+      .then(res => res.json())
+      .then(data => {
+        console.log('CreateReview: data fetched from /api/residents: ', data.residentsArray);
+        // Once we get all residents in DB, save in CreateReview state
+        this.setState({ residents: data.residentsArray });
+      })
+      .catch((e) => {
+        console.log('ERROR FETCHING RESIDENTS IN CreateReview cDM');
+        throw e;
+      })
   }
 
   onDelete(e) {
@@ -43,15 +63,19 @@ class ReviewCard extends Component {
   }
 
   render() {
-    return (<div className="reviewCard">
-      <h4>Week {this.props.week} Assessment: Review #{this.props.id}</h4><br />
-      <h4>Resident {this.props.residentId}</h4><br />
-      <p>{this.props.body}</p><br />
-      <div className="reviewCardButtons">
-        <button>Edit Review</button>
-        <button onClick={(e) => { this.onDelete(e) }}>Delete Review</button>
-      </div>
-    </div>);
+
+    if (this.state.residents) {
+      return (<div className="reviewCard">
+        <h4>Week {this.props.week} Assessment Review for {this.state.residents.filter(obj => obj._id === this.props.residentId)[0].username}</h4><br />
+        <p>{this.props.body}</p><br />
+        <div className="reviewCardButtons">
+          <button>Edit Review</button>
+          <button onClick={(e) => { this.onDelete(e) }}>Delete Review</button>
+        </div>
+      </div>);
+    } else {
+      return (<div>Please...wait....</div>);
+    }
   }
 }
 
